ea31c0d344f2da831f2a69516e4ebb4e880027e2
Forrest Klausmeier <fwklausmeier@gmail.com>
2021-03-13 19:02:06 +0000

    Exercises: documentation, loops, user input

---
+++ ruby_basics_exercises/loops/empty_the_array.rb
@@ -0,0 +1,20 @@
+# Given the array below, use loop to remove and print each name. Stop the
+# loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names.shift
+  break if names.size == 0
+end
+
+#----------------------or
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names.shift
+  break if names.empty?
+end
+
+
---
+++ ruby_basics_exercises/user_input/opposites_attract.rb
@@ -0,0 +1,5 @@
+# Write a program that requests two integers from the user, adds them together,
+# and then displays the result. Furthermore, insist that one of the integers be
+# positive, and one negative; however, the order in which the two integers are
+# entered does not matter.
+
