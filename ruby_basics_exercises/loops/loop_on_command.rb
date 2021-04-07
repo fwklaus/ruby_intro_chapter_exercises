d6aab5387dfa31e570d36c5e1ae2b9b9b02313b5
Forrest Klausmeier <fwklausmeier@gmail.com>
2021-04-07 16:38:45 +0000

    File edits & deletions

--- ruby_basics_exercises/loops/loop_on_command.rb
+++
@@ -1,9 +0,0 @@
-# Modify the code below so the loop stops iterating when the user inputs 'yes'.
-
-loop do
-  puts 'Should I stop looping?'
-  answer = gets.chomp
-  break if answer == 'yes'
-  puts "Wrong answer. Input 'yes' to exit"
-end
-
--- ruby_basics_exercises/loops/runaway_loop.rb
+++
@@ -1,6 +0,0 @@
-# Modify the code so the loop stops after the first iteration.
-
-loop do
-  puts 'Just keep printing...'
-  break
-end
